6aa80fcb15435ebdaf63c42ed8b0b7af062ac598
czz5 <3482917702@qq.com>
2022-07-31 17:47:59 +0800

    wrote a readme file

---
+++ git2.cpp
@@ -0,0 +1,16 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+using namespace std;
+int main()
+{
+	Mat image = imread("D://1.jpg");
+	namedWindow("高斯滤波【原图】");
+	namedWindow("高斯滤波【效果图】");
+	imshow("高斯滤波【原图】", image);
+	Mat out;
+	GaussianBlur(image, out, Size(5, 5), 0, 0);
+	imshow("高斯滤波【效果图】", out);
+	waitKey(0);
+	return 0;
+}
